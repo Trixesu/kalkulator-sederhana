cfb3c48a73e3e50c5e57bbd963b61256f479cb11
Trixesu <trixteresu@gmail.com>
2024-11-01 21:16:13 +0700

    Add files via upload

---
+++ KalkulatorSederhana.java
@@ -0,0 +1,10 @@
+package belajar.kalkulatorsederhana;
+
+import java.util.Scanner;
+
+public class KalkulatorSederhana {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
